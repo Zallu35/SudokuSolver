4edbd3b393dc0599d06776e5ccd34db74f3097bf
Zallu35 <s14.sboss@gmail.com>
2025-11-06 17:36:06 -0500

    Defining the parts of the board for future logic

---
+++ src/Board.py
@@ -0,0 +1,74 @@
+
+
+class Board:
+    def __init__(self):
+        self.complete = False
+        self.incomplete_rows = []
+        self.incomplete_boxes = []
+        self.incomplete_columns = []
+
+class Cell:
+    def __init__(self, row, column, box):
+        self.row = row
+        self.column = column
+        self.box = box
+        self.value = 0
+        self.potential = []
+    
+    def update_value(self, value):
+        self.value = value
+        self.potential.clear()
+
+    def add_potentials(self, potentials_list):
+        self.potential = potentials_list
+    
+    def remove_potential(self, value):
+        self.potential.remove(value)
+
+class Row:
+    def __init__(self):
+        self.boxes = []
+        self.columns = []
+        self.cells = []
+        self.empty = [] #list of empty cells (value = 0)
+    
+    def fill_boxes(self, box):
+        self.boxes.append(box)
+
+    def fill_columns(self, column):
+        self.columns.append(column)
+
+    def fill_cells(self, cell):
+        self.cells.append(cell)
+
+class Box:
+    def __init__(self):
+        self.rows = []
+        self.columns = []
+        self.cells = []
+        self.empty = [] #list of empty cells (value = 0)
+
+    def fill_rows(self, row):
+        self.rows.append(row)
+
+    def fill_columns(self, column):
+        self.columns.append(column)
+
+    def fill_cells(self, cell):
+        self.cells.append(cell)
+
+class Column:
+    def __init__(self):
+        self.rows = []
+        self.boxes = []
+        self.cells = []
+        self.empty = [] #list of empty cells (value = 0)
+
+    def fill_boxes(self, box):
+        self.boxes.append(box)
+
+    def fill_rows(self, row):
+        self.rows.append(row)
+
+    def fill_cells(self, cell):
+        self.cells.append(cell)
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import Board
+
+def main():
+
+
+
+
+main()
