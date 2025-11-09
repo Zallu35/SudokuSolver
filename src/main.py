ab327619ff1c8e5fde148b33aaee424ceaeffdc0
Zallu35 <s14.sboss@gmail.com>
2025-11-09 13:49:13 -0500

    Created sudoku grid and some test buttons

--- src/main.py
+++ src/main.py
@@ -1,21 +1,62 @@
 import Board
-import tkinter as tk
+import re
+from tkinter import *
+from tkinter import ttk
 
 def main():
-    #creating board elements
-    Gameboard = Board()
-    Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9 = Row(), Row(), Row(), Row(), Row(), Row(), Row(), Row(), Row()
-    Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8, Column9 = Column(), Column(), Column(), Column(), Column(), Column(), Column(), Column(), Column()
-    Box1, Box2, Box3, Box4, Box5, Box6, Box7, Box8, Box9 = Box(), Box(), Box(), Box(), Box(), Box(), Box(), Box(), Box()
-    Cell11, Cell12, Cell13, Cell14, Cell15, Cell16, Cell17, Cell18, Cell19 = Cell(Row1, Column1, Box1), Cell(Row1, Column2, Box1), Cell(Row1, Column3, Box1), Cell(Row1, Column4, Box2), Cell(Row1, Column5, Box2), Cell(Row1, Column6, Box2), Cell(Row1, Column7, Box3), Cell(Row1, Column8, Box3), Cell(Row1, Column9, Box3)
-    Cell21, Cell22, Cell23, Cell24, Cell25, Cell26, Cell27, Cell28, Cell29 = Cell(Row2, Column1, Box1), Cell(Row2, Column2, Box1), Cell(Row2, Column3, Box1), Cell(Row2, Column4, Box2), Cell(Row2, Column5, Box2), Cell(Row2, Column6, Box2), Cell(Row2, Column7, Box3), Cell(Row2, Column8, Box3), Cell(Row2, Column9, Box3)
-    Cell31, Cell32, Cell33, Cell34, Cell35, Cell36, Cell37, Cell38, Cell39 = Cell(Row3, Column1, Box1), Cell(Row3, Column2, Box1), Cell(Row3, Column3, Box1), Cell(Row3, Column4, Box2), Cell(Row3, Column5, Box2), Cell(Row3, Column6, Box2), Cell(Row3, Column7, Box3), Cell(Row3, Column8, Box3), Cell(Row3, Column9, Box3)
-    Cell41, Cell42, Cell43, Cell44, Cell45, Cell46, Cell47, Cell48, Cell49 = Cell(Row4, Column1, Box4), Cell(Row4, Column2, Box4), Cell(Row4, Column3, Box4), Cell(Row4, Column4, Box5), Cell(Row4, Column5, Box5), Cell(Row4, Column6, Box5), Cell(Row4, Column7, Box6), Cell(Row4, Column8, Box6), Cell(Row4, Column9, Box6)
-    Cell51, Cell52, Cell53, Cell54, Cell55, Cell56, Cell57, Cell58, Cell59 = Cell(Row5, Column1, Box4), Cell(Row5, Column2, Box4), Cell(Row5, Column3, Box4), Cell(Row5, Column4, Box5), Cell(Row5, Column5, Box5), Cell(Row5, Column6, Box5), Cell(Row5, Column7, Box6), Cell(Row5, Column8, Box6), Cell(Row5, Column9, Box6)
-    Cell61, Cell62, Cell63, Cell64, Cell65, Cell66, Cell67, Cell68, Cell69 = Cell(Row6, Column1, Box4), Cell(Row6, Column2, Box4), Cell(Row6, Column3, Box4), Cell(Row6, Column4, Box5), Cell(Row6, Column5, Box5), Cell(Row6, Column6, Box5), Cell(Row6, Column7, Box6), Cell(Row6, Column8, Box6), Cell(Row6, Column9, Box6)
-    Cell71, Cell72, Cell73, Cell74, Cell75, Cell76, Cell77, Cell78, Cell79 = Cell(Row7, Column1, Box7), Cell(Row7, Column2, Box7), Cell(Row7, Column3, Box7), Cell(Row7, Column4, Box8), Cell(Row7, Column5, Box8), Cell(Row7, Column6, Box8), Cell(Row7, Column7, Box9), Cell(Row7, Column8, Box9), Cell(Row7, Column9, Box9)
-    Cell81, Cell82, Cell83, Cell84, Cell85, Cell86, Cell87, Cell88, Cell89 = Cell(Row8, Column1, Box7), Cell(Row8, Column2, Box7), Cell(Row8, Column3, Box7), Cell(Row8, Column4, Box8), Cell(Row8, Column5, Box8), Cell(Row8, Column6, Box8), Cell(Row8, Column7, Box9), Cell(Row8, Column8, Box9), Cell(Row8, Column9, Box9)
-    Cell91, Cell92, Cell93, Cell94, Cell95, Cell96, Cell97, Cell98, Cell99 = Cell(Row9, Column1, Box7), Cell(Row9, Column2, Box7), Cell(Row9, Column3, Box7), Cell(Row9, Column4, Box8), Cell(Row9, Column5, Box8), Cell(Row9, Column6, Box8), Cell(Row9, Column7, Box9), Cell(Row9, Column8, Box9), Cell(Row9, Column9, Box9)
+    
+    def p_vals():
+        for C in Cell_list:
+            if C.get():
+                print(f"{C}:{C.get()}")
+
+    def update_entry():
+        Cell_list[40].insert(0, 5)
+    
+    def clear_board():
+        for Cell in Cell_list:
+            Cell.delete(0)
+    
+    def entry_restriction(value):
+        return re.match('^[0-9]*$', value) is not None and len(value) <2
+    
+        
+    root = Tk()
+    root.title("Sudoku Solver")
+    sudoku_board = ttk.Frame(root)
+    sudoku_board.grid(column=0, row=0, padx=4, pady=4, sticky=(N,W,E,S))
+    solve = ttk.Button(root, text = "Solve", command=update_entry)
+    solve.grid(column=0, row=1, sticky=(E))
+    clear_board = ttk.Button(root, text="Clear", command=clear_board)
+    clear_board.grid(column=0, row=1, sticky=(W))
+    test_vals = ttk.Button(root, text="Vals", command=p_vals)
+    test_vals.grid(column=0, row=1)
+    valid_entry = (root.register(entry_restriction), '%P')
+    
+    Cell_list = []
+    Value_list = []
+    for i in range(1, 10): #row
+        for j in range(1, 10): #column
+            ValueID = StringVar()
+            Value_list.append(ValueID)
+            CellID = ttk.Entry(sudoku_board, justify = "center", width=3, textvariable=ValueID, validate='key', validatecommand=valid_entry)
+            CellID.grid(column=j, row=i)
+            Cell_list.append(CellID)
+            
+
+    #for v in Value_list:
+        #pos=list(v)
+        #c = ttk.Entry(sudoku_board, justify = "center", width=3, textvariable=v)
+        #c.grid(column=int(pos[3]), row=int(pos[2]))
+        #Cell_list.append(c)
+    # CV=StringVar()
+    #C=ttk.Entry(sudoku_board, textvariable=CV)
+    #C.grid(column=0, row=0)
+    #print(Cell_list[0].configure())
+    #print(Value_list)
+    root.mainloop() 
+
+    
     
 
 
